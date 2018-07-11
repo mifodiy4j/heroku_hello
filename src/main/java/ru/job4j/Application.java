74c3a6dbe95f41dddead1d35edff316c2b1dcd82
Mikhailov Sergey <mifodiy15@gmail.com>
2018-07-11 16:15:57 +0300

    change problem white label

--- src/main/java/ru/job4j/Application.java
+++ src/main/java/ru/job4j/Application.java
@@ -2,7 +2,9 @@ package ru.job4j;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan(basePackages={"web"})
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
